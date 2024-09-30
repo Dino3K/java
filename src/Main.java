c4df20c6892e4ff4bc1d09acdf414f868ba52fb8
unknown <barragan5907@gmail.com>
2024-09-30 13:13:40 -0500

    Ahi va todo

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("Que onda pa");
+        System.out.println(":D");
+
+        int n1 = scanner.nextInt();
+        int n2 = scanner.nextInt();
+
+        int sum = n1 + n2;
+
+        System.out.println(sum);
+    }
+}
