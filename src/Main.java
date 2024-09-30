7c65df4174966dd674f4be523ecaa9d83d0af182
unknown <barragan5907@gmail.com>
2024-09-30 14:38:23 -0500

    ahi va toh

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
@@ -7,6 +10,8 @@ public class Main {
         Scanner scanner =new Scanner(System.in);
         System.out.println("Que onda pa");
         System.out.println(":D");
+        System.out.println("Digame su nombre c:");
+        String name = scanner.nextLine();
 
         int n1 = scanner.nextInt();
         int n2 = scanner.nextInt();
@@ -14,5 +19,18 @@ public class Main {
         int sum = n1 + n2;
 
         System.out.println(sum);
+        if (sum == 3) {
+            System.out.println("Tonto");
+
+
+        } else {
+            System.out.println("Nose c:");
+        }
+        if (Objects.equals(name, "nicolah")) {
+            System.out.println("Hola nicolah");
+
+        } else {
+            System.out.println("Ola "+name);
+        }
     }
 }
---
+++ src/Pass.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.Objects;
+public class Pass {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String password;
+        int intentos = 0;
+
+        do {
+            System.out.println("Ingrese la contraseña");
+            password = scanner.nextLine();
+            intentos++;
+        } while (!password.equals("Tonto123") && intentos < 5);
+        if (password.equals("Tonto123")){
+            System.out.println("Goog choice");
+        } else {
+            System.out.println("Mamaste");
+        }
+
+
+
+
+    }
+}
