c8c6e15b95a3abc45f6c6782524df510df87e8ee
unknown <barragan5907@gmail.com>
2024-10-03 16:07:10 -0500

    Arreglitos

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,49 @@
-import org.w3c.dom.ls.LSOutput;
-
-import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
-
-
     public static void main(String[] args) {
-        Scanner scanner =new Scanner(System.in);
-        System.out.println("Que onda pa");
-        System.out.println(":D");
-        System.out.println("Digame su nombre c:");
-        String name = scanner.nextLine();
-
-        int n1 = scanner.nextInt();
-        int n2 = scanner.nextInt();
-
-        int sum = n1 + n2;
-
-        System.out.println(sum);
-        if (sum == 3) {
-            System.out.println("Tonto");
-
-
-        } else {
-            System.out.println("Nose c:");
-        }
-        if (Objects.equals(name, "nicolah")) {
-            System.out.println("Hola nicolah");
-
-        } else {
-            System.out.println("Ola "+name);
+        Autobus Bus = new Autobus();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hola!");
+        int num = 1;
+        while (num == 1){
+            System.out.println("Que quieres hacer?");
+            System.out.println("1: Reccoger Pasajero");
+            System.out.println("2: Descargar Pasajero");
+            System.out.println("3: Tanquear");
+            System.out.println("4: Arrancar");
+            int op = sc.nextInt();
+            switch (op){
+                case 1:
+                    System.out.println("Cuantos pasajeros?");
+                    int ps = sc.nextInt();
+                    Bus.recogerP(ps);
+                    break;
+                case 2:
+                    System.out.println("Cuantos pasajeros?");
+                    int pss = sc.nextInt();
+                    Bus.descargarP(pss);
+                    break;
+                case 3:
+                    if (Bus.TanqCombustible == 100){
+                        System.out.println("Tanque lleno");
+                        break;
+                    }else {
+                        System.out.println("Cuanto quiere tanquear?");
+                        int tnq = sc.nextInt();
+                        Bus.Tanquear(tnq);
+                        break;
+                    }
+                case 4:
+                    if(Bus.TanqCombustible < 5){
+                        System.out.println("Debe tanquear primero");
+                        break;
+                    }else{
+                        System.out.println("Nos fuimonos ;D!");
+                        num++;
+                        break;
+                    }
+            }
         }
     }
 }
